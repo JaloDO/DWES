2234953bf21e11b7ca32b19a76f1adf966dab7ad
JaloDO <jaliyodiaz96@gmail.com>
2020-09-25 10:13:04 +0200

    appweb4 0.4.4

--- appweb4/ejercicio3/ej3.php
+++ appweb4/ejercicio3/ej3.php
@@ -4,27 +4,18 @@
     require_once '../datos/datos.php';
     require_once '../datos/cabecera.php';
     
-    $lista_departamentos = [];
     $empleados_departamento = [];
 
-    foreach($datos as $dept) {
-            
-        if(!in_array($dept["DEPARTAMENTO"],$lista_departamentos)){
-            $lista_departamentos[] =  $dept["DEPARTAMENTO"];
-        }   
-    }
-    /*..................................................................*/ 
-
     foreach($datos as $empresa){
-        $dept_name = $empresa["DEPARTAMENTO"];
-        echo "ID: ".$empresa["ID"];
+        
+       if(array_key_exists($empresa["DEPARTAMENTO"], $empleados_departamento)){
+            $nEmple = $empleados_departamento[$empresa["DEPARTAMENTO"]];
+            $nEmple += $empresa["EMPLEADOS"];
 
-        for($i=0; $i<sizeof($lista_departamentos); $i++){
-            echo "Registro: ".$i;
-            if($dept_name == $lista_departamentos[$i]){
-                $auxiliar = [$dept_name => $empresa["EMPLEADOS"]];
-        }
-        $empleados_departamento = array_merge($empleados_departamento, $auxiliar);
+       }else{
+            $empleados_departamento [] = [$empresa["DEPARTAMENTO"] => $empresa["EMPLEADOS"]];
+       }
+        
     }
 
     /**
@@ -35,9 +26,17 @@
     $html="<div class='container'>";
     $html = $html."<table class='table table-responsive'>";
     $html = $html."<tr><th>DEPARTAMENTO</th><th>EMPLEADOS</th></tr>";
+
+    while(current($empleados_departamento)){
+        $html = $html."<tr>";
+        $html = $html."<td>".key($empleados_departamento)."</td>";
+        $html = $html."<td>".$empleados_departamento."</td>";
+        $html = $html."</tr>";
+        next($empleados_departamento);
+    }
+    $html = $html."</table></body></html>";
     
-    echo $head;
-    echo "<pre>";
-        print_r($empleados_departamento);
-    echo"</pre></div></body></html>";
+    echo $head, $html;
+    print_r($empleados_departamento);
+  
         
