ea9ef1c0704df3e7ea73aaa95355ee141d8ffea1
JaloDO <jaliyodiaz96@gmail.com>
2020-09-25 09:42:25 +0200

    appweb4 0.4.4

--- appweb4/ejercicio3/ej3.php
+++ appweb4/ejercicio3/ej3.php
@@ -3,44 +3,41 @@
     /*MY CODE HERE*/
     require_once '../datos/datos.php';
     require_once '../datos/cabecera.php';
-        
-    $dept = [["Departamento" => "Contabilidad", "Empleados" => 0]];
-    $emple = [];
-    $cont=0;
     
-    foreach($datos as $empresa) {
+    $lista_departamentos = [];
+    $empleados_departamento = [];
 
-        echo "Entrada:".++$cont." ";
-        $cont2=0;
-        foreach($dept as $name){
+    foreach($datos as $dept) {
+            
+        if(!in_array($dept["DEPARTAMENTO"],$lista_departamentos)){
+            $lista_departamentos[] =  $dept["DEPARTAMENTO"];
+        }   
+    }
+    /*..................................................................*/ 
 
-            echo "Entrada: ".$cont.".".++$cont2." ";
+    foreach($datos as $empresa){
+        $dept_name = $empresa["DEPARTAMENTO"];
+        echo "ID: ".$empresa["ID"];
 
-            if($empresa["DEPARTAMENTO"]==$name["Departamento"]){
-               $emple = ["Departamento" => $name["Departamento"], "Empleados" => $empresa["EMPLEADOS"]];
-               $dept = array_merge($dept, $emple);
-                break; 
-            }
-            $dept[] = ["Departamento" => $name["Departamento"], "Empleados" => $empresa["EMPLEADOS"]]; 
+        for($i=0; $i<sizeof($lista_departamentos); $i++){
+            echo "Registro: ".$i;
+            if($dept_name == $lista_departamentos[$i]){
+                $auxiliar = [$dept_name => $empresa["EMPLEADOS"]];
         }
+        $empleados_departamento = array_merge($empleados_departamento, $auxiliar);
     }
 
     /**
-     * Aqui haré un for para recorrer la lista dept["DEPARTAMENTO","EMPLEADOS"]
-     * en una tabla
+     * AQUI TENGO QUE HACER EL WHILW
+     * PERO PRIMERO, TENGO QUE ARREGLAR EL FINAL DE MI ARCHIVO
      */
-
+    
     $html="<div class='container'>";
     $html = $html."<table class='table table-responsive'>";
     $html = $html."<tr><th>DEPARTAMENTO</th><th>EMPLEADOS</th></tr>";
-
-    foreach($dept as $row){
-        $html = $html."<tr>";
-        $html = $html."<td>".$row["Departamento"]."</td>";
-        $html = $html."<td>".$row["Empleados"]."</td>";
-        $html = $html."</tr>";
-    }
-
-    $html = $html."</table></div></body></html>";
-    echo $head,$html;
+    
+    echo $head;
+    echo "<pre>";
+        print_r($empleados_departamento);
+    echo"</pre></div></body></html>";
         
